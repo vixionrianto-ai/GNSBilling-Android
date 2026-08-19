d9665c245d45407b88d3ddd6f8fd41fcb08bb0c1
vixionrianto-ai <vixionrianto@gmail.com>
2026-08-19 18:10:59 +0700

    Add exact PPP Profile update operation to repository

--- app/src/main/java/com/gns/billing/repository/RouterRepository.kt
+++ app/src/main/java/com/gns/billing/repository/RouterRepository.kt
@@ -6,7 +6,6 @@ class RouterRepository {
     suspend fun getRouter() = RetrofitClient.api.getRouter()
     suspend fun getProfiles(routerId: Int) = RetrofitClient.api.getProfiles(routerId)
     suspend fun testRouter(routerId: Int) = RetrofitClient.api.testRouter(routerId)
-
     suspend fun getPppSecret(routerId: Int) = RetrofitClient.api.getPppSecret(routerId)
     suspend fun createSecret(routerId: Int, username: String, password: String, service: String, profile: String) = RetrofitClient.api.createSecret(routerId, username, password, service, profile)
     suspend fun editSecret(routerId: Int, username: String) = RetrofitClient.api.editSecret(routerId, username)
@@ -14,8 +13,8 @@ class RouterRepository {
     suspend fun deleteSecret(routerId: Int, secret: String) = RetrofitClient.api.deleteSecret(routerId, secret)
     suspend fun enableSecret(routerId: Int, secret: String) = RetrofitClient.api.enableSecret(routerId, secret)
     suspend fun disableSecret(routerId: Int, secret: String) = RetrofitClient.api.disableSecret(routerId, secret)
-
     suspend fun getPppProfile(routerId: Int) = RetrofitClient.api.getPppProfile(routerId)
     suspend fun createProfile(routerId: Int, name: String, localAddress: String, remoteAddress: String, rateLimit: String, onlyOne: String) = RetrofitClient.api.createProfile(routerId, name, localAddress, remoteAddress, rateLimit, onlyOne)
+    suspend fun updateProfile(routerId: Int, profile: String, name: String, localAddress: String, remoteAddress: String, rateLimit: String, onlyOne: String) = RetrofitClient.api.updateProfile(routerId, profile, name, localAddress, remoteAddress, rateLimit, onlyOne)
     suspend fun deleteProfile(routerId: Int, profile: String) = RetrofitClient.api.deleteProfile(routerId, profile)
 }
